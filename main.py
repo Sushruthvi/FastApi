e24b192c01a914733493bd75d88032383965160d
Sushruthvi <sushruthvi3@gmail.com>
2025-06-23 01:04:46 +0530

    make root api,about api,view api to view the pateint data

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 app=FastAPI()
 
+def load_data():
+  with open('pateints.json','r') as f:
+     data=json.load(f)
+     return data;
+
 @app.get("/")
 def hello():
-  return {"message":"hello world"};
+  return {"message":"This is patient management system API"};
 
 @app.get("/about")
 def about():
-  return {"About":"I am sushruth who is learning FastApi"}
+  return {"About":"A fully functional API to manage pateint records"}
+
+@app.get("/view")
+def view():
+ data=load_data()
+ return data;
